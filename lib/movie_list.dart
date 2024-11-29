20bae024bb16505fe7c1d3a5f6915ee4d6c61ff5
anasassad <lwalida1921@gmail.com>
2024-11-29 13:15:06 +0100

    Showing a trailing icon in a ListTile

--- lib/movie_list.dart
+++ lib/movie_list.dart
@@ -9,6 +9,10 @@ class MovieList extends StatefulWidget {
 }
 
 class _MovieListState extends State<MovieList> {
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+
   late HttpHelper httpHelper;
   late int moviesCount;
   late List movies;
@@ -33,6 +37,8 @@ class _MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
+    NetworkImage image;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -44,10 +50,18 @@ class _MovieListState extends State<MovieList> {
         body: ListView.builder(
             itemCount: moviesCount,
             itemBuilder: (BuildContext context, int position) {
+              if (movies[position].posterPath != null) {
+                image = NetworkImage('$iconBase${movies[position].posterPath}');
+              } else {
+                image = NetworkImage(defaultImage);
+              }
               return Card(
                 color: Colors.white,
                 elevation: 1.0,
                 child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: image,
+                  ),
                   title: Text(movies[position].title),
                   subtitle: Text(
                       'Released:  ${movies[position].releaseDate} - Vote: ${movies[position].voteAverage.toString()}'),
